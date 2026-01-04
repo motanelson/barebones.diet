d919eda83e67390d695c783d7e0862f21cbace9a
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-01-04 12:29:45 +0000

    Add files via upload

--- hello.c
+++ hello.c
@@ -1,9 +1,16 @@
+
+//sudo apt install dietlibc-dev
+//gcc -m32 -nostdlib -fno-pie -no-pie hello.c -L/usr/lib/diet -lc -lgcc -o hello
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
     char *c="hello world...\n";
     puts(c);
-    exit(0);
+    
     return 0;
 
+}
+void _start(){
+    main();
+    exit(0);
 }
